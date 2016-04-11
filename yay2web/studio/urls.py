a0746eb54065ec70e07d0535ec79cd30d5565870
Koen Martens <gmc@sonologic.nl>
2016-04-11 20:12:14 +0200

    add api views for alsa info

--- yay2web/studio/urls.py
+++ yay2web/studio/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^/?$', views.index, name='dashboard'),
     url(r'status', views.status, name='status'),
+    url(r'cards', views.cards, name='cards'),
+    url(r'pcms', views.pcms, name='pcms'),
 ]
 
--- yay2web/studio/views.py
+++ yay2web/studio/views.py
@@ -1,14 +1,36 @@
 from time import time
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
 import json
+import alsaaudio
 
 from django.contrib.auth.decorators import login_required
 
+# html views
+
 @login_required
 def index(request):
-    context = {}
+    context = RequestContext(request, {
+    })
     return render(request, 'studio/dashboard.html', context)
+
+def sinks(request):
+    context = RequestContext(request, {
+    })
+    return render(request, 'studio/sinks.html', context)
+    
+#@login_required
+#def config_sink_icecast(request):
+#    context = {}
+#    return render(request, 'studio/config_sink.html', context)
+#
+#@login_required
+#def config_source_alsa(request):
+#    context = {}
+#    return render(request, 'studio/config_sink.html', context)
+
+# json views (todo, split to api ap)
 
 @login_required
 def status(request):
@@ -16,3 +38,15 @@ def status(request):
         't' : time(),
     }
     return HttpResponse(json.dumps(data), content_type="application/json")
+
+@login_required
+def cards(request):
+    data = alsaaudio.cards()
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+@login_required
+def pcms(request):
+    data = alsaaudio.pcms()
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
